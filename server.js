c1fa28b960d0c11199cff377b0ce772ddce140c2
Cesar-Quintero <cesarqdev@gmail.com>
2022-08-18 16:12:17 -0700

    put PORT inside env file

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
 require('dotenv').config({ path: './config/.env' })
 
 let db,
