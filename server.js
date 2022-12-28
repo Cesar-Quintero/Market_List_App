e682539015556de49152eab4c09b9d90cb9586f2
Cesar-Quintero <cesarqdev@gmail.com>
2022-12-28 16:12:53 -0700

    debug connection to mdb revert change

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
-        console.log(client)
     })
 
 app.set('view engine', 'ejs')
